80e43f672ba8da8ccf405934b7cdf9a4d12b1aa4
babaledeb <bastien_debras@hotmail.com>
2014-03-10 15:55:01 +0100

    Update testTFCT.m

--- testTFCT.m
+++ testTFCT.m
@@ -1,13 +1,18 @@
-%[x, fs] = wavread('sample.wav');
-fs = 44100;
-x = zeros(2*fs,1);
-for i=1:2*fs
-    if (i<fs)
-        x(i) = cos(2*pi*1000*i/fs);
-    else
-        x(i) = cos(2*pi*2000*i/fs);
-    end
-end
-sound(x, fs);
-wlen = 0.050*fs; % Fenetre de 50ms
-tfct(x, fs, wlen, wlen/4, 1);
+[x, fs] = wavread('Chrom1.5.wav');
+x = x(:,1);
+%fs = 44100;
+%x = zeros(2*fs,1);
+%for i=1:2*fs
+   % if (i<fs)
+     %   x(i) = cos(2*pi*1000*i/fs);
+    %else
+      %  x(i) = cos(2*pi*2000*i/fs);
+  %  end
+%end
+%sound(x, fs);
+wlen = 0.05*fs; % Fenetre de 50ms
+W = makeW(x, fs, wlen, wlen/4,46);
+
+imagesc(W);
+axis xy;
+title('Original H');
